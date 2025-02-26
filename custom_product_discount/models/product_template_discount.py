218f1a5935f08a5bd5af0cfdf9b15517e27f48a7
Surbhi <surbhijakasaniya2@gmail.com>
2025-02-26 16:57:10 +0530

    override base method for show discount price on ui for product list view

--- custom_product_discount/models/product_template_discount.py
+++ custom_product_discount/models/product_template_discount.py
@@ -47,6 +47,73 @@ class custom_product_discount(models.Model):
         return res
 
 
+    # for show discount price on list view of product, override below method
+    def _get_sales_prices(self, pricelist, fiscal_position):
+        """
+            only updated condition for count base price when we have discount in our product is set - discount_percentage
+
+            calculate base price based on original_price rather than list_price
+        """
+        if not self:
+            return {}
+
+        pricelist and pricelist.ensure_one()
+        pricelist = pricelist or self.env['product.pricelist']
+        currency = pricelist.currency_id or self.env.company.currency_id
+        date = fields.Date.context_today(self)
+
+        sales_prices = pricelist._get_products_price(self, 1.0)
+        show_discount = pricelist and pricelist.discount_policy == 'without_discount'
+        show_strike_price = self.env.user.has_group('website_sale.group_product_price_comparison')
+
+        base_sales_prices = self._price_compute('list_price', currency=currency)
+
+        res = {}
+        for template in self:
+            price_reduce = sales_prices[template.id]
+
+            product_taxes = template.sudo().taxes_id._filter_taxes_by_company(self.env.company)
+            taxes = fiscal_position.map_tax(product_taxes)
+
+            base_price = None
+            price_list_contains_template = currency.compare_amounts(price_reduce, base_sales_prices[template.id]) != 0
+
+            if template.compare_list_price and show_strike_price:
+                # The base_price becomes the compare list price and the price_reduce becomes the price
+                base_price = template.compare_list_price
+                if not price_list_contains_template:
+                    price_reduce = base_sales_prices[template.id]
+
+                if template.currency_id != currency:
+                    base_price = template.currency_id._convert(
+                        base_price,
+                        currency,
+                        self.env.company,
+                        date,
+                        round=False
+                    )
+
+            elif template.discount_percentage:
+                # Compare_list_price are never tax included
+                base_price = self._apply_taxes_to_price(
+                    template.original_price, currency, product_taxes, taxes, template,
+                )
+
+            price_reduce = self._apply_taxes_to_price(
+                price_reduce, currency, product_taxes, taxes, template,
+            )
+
+            template_price_vals = {
+                'price_reduce': price_reduce,
+            }
+            if base_price:
+                template_price_vals['base_price'] = base_price
+
+            res[template.id] = template_price_vals
+
+        return res
+
+
 class custom_product_product_discont(models.Model):
     _inherit = 'product.product'
 
